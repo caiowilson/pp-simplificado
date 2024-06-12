8101e784e9717407608bb3f0478c5eb838943057
Caio Wilson <caiob@xgrow.com>
2024-06-12 00:35:45 -0300

    chore: Update AppServiceProvider with permission gate definitions and custom notification channel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
+use App\Broadcasting\CustomChannel;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Notifications\ChannelManager;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Notifications\NotificationChannel;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // $this->app->bind(\Faker\Generator::class . ':' . config('app.faker_locale'), \Faker\Generator::class);
     }
 
     /**
@@ -19,6 +26,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        try {
+            foreach (Permission::pluck('name') as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    return $user->roles()->whereHas('permissions', function (Builder $q) use ($permission) {
+                        $q->where('name', $permission);
+                    })->exists();
+                });
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            //avoid error when booting before running migrations
+        }
+    
+        Notification::resolved(function (ChannelManager $service) {
+            $service->extend('custom', function ($app) {
+                return new CustomChannel();
+            });
+        });
     }
 }
